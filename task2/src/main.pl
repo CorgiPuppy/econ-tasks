09291ff03af2fc3cef02bac732a8b77f65d6cec6
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2024-10-18 01:18:31 +0300

    fix: add values.

--- task2/src/main.pl
+++ task2/src/main.pl
@@ -8,35 +8,34 @@ use Expenses;
 use Income;
 use Report;
 
-print("Таблица расходов и доходов личного бюджета на 2 недели\n");
-
 my $expenses = Expenses->new({
-							 household_needs => [2323, 23],
-							 bad_habits => [2132],
-							 hygiene_and_health => [32],
-							 clothes => [32],
-							 trips => [32],
-							 food_products => [32],
-							 entertainments => [32],
-							 connection => [32],
-							 total_per_week => [32],
-							 total_per_day => [32]});
+							 household_needs => [456, 500, 0, 456, 0, 0, 0, 0, 0],
+							 bad_habits => [0, 0, 0, 0, 0, 0, 0, 0, 0],
+							 hygiene_and_health => [37100, 35000, 0, 0, 0, 0, 0, 4500, 32600],
+							 clothes => [0, 0, 0, 0, 0, 0, 0, 0, 0],
+							 trips => [0, 0, 0, 0, 0, 0, 0, 0, 0],
+							 food_products => [785, 1500, 67, 470, 90, 0, 158, 0, 0],
+							 entertainments => [0, 0, 0, 0, 0, 0, 0, 0, 0],
+							 connection => [0, 0, 0, 0, 0, 0, 0, 0, 0],
+							 total_per_week => [38341, 37000],
+							 total_per_day => [67, 926, 90, 0, 158, 4500, 32600]});
 
 my $income = Income->new({
-						 salary => 1000,
-						 parents_money => 1000,
-						 total => 1000 + 1000});
+						 salary => 1579,
+						 parents_money => 0,
+						 total => 1579 + 0});
 
 my $report = Report->new({
-						 weekly_income => 1000,
-						 weekly_expenses => 100,
-						 balance => 10,
-						 expenses_plan => 50,
-						 departure_plan => 450});
+						 weekly_income => 1579,
+						 weekly_expenses => 38341,
+						 balance => 36762,
+						 expenses_plan => 37000,
+						 departure_plan => 258});
 
-my $expenses_file = "../files/expenses.txt";
-my $income_file = "../files/income.txt";
-my $report_file = "../files/report.txt";
+my $folder = "../files/";
+my $expenses_file = $folder . "expenses.txt";
+my $income_file = $folder . "income.txt";
+my $report_file = $folder . "report.txt";
 
 $expenses->write_to_file($expenses_file);
 $income->write_to_file($income_file);
