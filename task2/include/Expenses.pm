18fa7d8449a8c27056a0c5b0593dbb9e16f64d55
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2024-10-18 01:01:34 +0300

    fix: modify print formatting.

--- task2/include/Expenses.pm
+++ task2/include/Expenses.pm
@@ -28,22 +28,23 @@ sub print_array
 sub write_to_file
 {
 	my $self = shift;
+	my $notice = "Примечание:\n" . "\tР.нед. - расходы на неделю;\n" . "\tП.р. - план расходов.\n\n";
 	my $title = "Расходы бюджета\n";
-	my $columns = "Категории расходов" . "\t" . "Расходы за неделю" . "\t" . "План расходов" . "\t" . "1" . "\t" . "2". "\t" . "3" . "\t" . "4" . "\t" . "5" . "\t" . "6" . "\t" . "7\n";
-	my $household_needs_column = "Бытовые нужды" . "\t\t" . $self->print_array($self->{household_needs}, "\t\t\t") . "\n";
-	my $bad_habits_column = "Вредные привычки" . "\t" . $self->print_array($self->{bad_habits}, "\t\t") . "\n";
-	my $hygiene_and_health_column = "Гигиена и здоровье" . "\t" . $self->print_array($self->{hygiene_and_health}, "\t\t") . "\n";
-	my $clothes_column = "Одежда" . "\t" . $self->print_array($self->{clothes}, "\t\t") . "\n";
-	my $trips_column = "Поездки" . "\t" . $self->print_array($self->{trips}, "\t\t") . "\n";
-	my $food_products_column = "Продукты питания" . "\t" . $self->print_array($self->{food_products}, "\t\t") . "\n";
-	my $entertainments_column = "Развлечения" . "\t" . $self->print_array($self->{entertainments}, "\t\t") . "\n";
-	my $connection_column = "Связь" . "\t" . $self->print_array($self->{connection}, "\t\t") . "\n";
-	my $total_per_week_column = "Итого за неделю" . "\t\t\t" . $self->print_array($self->{total_per_week}, "\t\t") . "\n";	
-	my $total_per_day_column = "Итого за день" . "\t\t\t" . $self->print_array($self->{total_per_day}, "\t\t") . "\n";
+	my $columns = "Категории расходов" . "\t" . "Р.нед." . "\t" . "П.р." . "\t" . "1" . "\t" . "2". "\t" . "3" . "\t" . "4" . "\t" . "5" . "\t" . "6" . "\t" . "7\n";
+	my $household_needs_column = "Бытовые нужды" . "\t\t" . $self->print_array($self->{household_needs}, "\t") . "\n";
+	my $bad_habits_column = "Вредные привычки" . "\t" . $self->print_array($self->{bad_habits}, "\t") . "\n";
+	my $hygiene_and_health_column = "Гигиена и здоровье" . "\t" . $self->print_array($self->{hygiene_and_health}, "\t") . "\n";
+	my $clothes_column = "Одежда" . "\t\t\t" . $self->print_array($self->{clothes}, "\t") . "\n";
+	my $trips_column = "Поездки" . "\t\t\t" . $self->print_array($self->{trips}, "\t") . "\n";
+	my $food_products_column = "Продукты питания" . "\t" . $self->print_array($self->{food_products}, "\t") . "\n";
+	my $entertainments_column = "Развлечения" . "\t\t" . $self->print_array($self->{entertainments}, "\t") . "\n";
+	my $connection_column = "Связь" . "\t\t\t" . $self->print_array($self->{connection}, "\t") . "\n";
+	my $total_per_week_column = "Итого за неделю" . "\t\t" . $self->print_array($self->{total_per_week}, "\t") . "\n";	
+	my $total_per_day_column = "\t\t\t" . "Итого за день" . "\t" . $self->print_array($self->{total_per_day}, "\t") . "\n";
 	
 	my ($filename) = @_;
 	open(FH, '>', $filename) or die $!;
-	print FH $title . $columns . $household_needs_column . $bad_habits_column . $hygiene_and_health_column . $clothes_column . $trips_column . $food_products_column . $entertainments_column . $connection_column . $total_per_week_column . $total_per_day_column;
+	print FH $notice . $title . $columns . $household_needs_column . $bad_habits_column . $hygiene_and_health_column . $clothes_column . $trips_column . $food_products_column . $entertainments_column . $connection_column . $total_per_week_column . $total_per_day_column;
 	close(FH);
 }
 
